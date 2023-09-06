74977396e15a6f4ff348843879bd7a1fb0c9203e
yash8877 <69146807+yash8877@users.noreply.github.com>
2023-09-06 19:48:41 +0530

    depart_problem.cpp

--- cd.cpp
+++ cd.cpp
@@ -1,39 +1,3 @@
-// #include <iostream>
-// #include<string>
-// using namespace std;
-// class Occurence{
-// 	private:
-// 	string str;
-// 	char ch;
-// 	public:
-// 	void search(string str,char ch){
-// 		int count=0;
-// 		int len = str.length();
-// 		for (int i = 0; i <= len; i++)
-// 		{
-// 			if (ch == str[i])
-// 			{
-// 				count++;
-// 			}
-// 		}
-// 		cout<<count;
-// 	}
-
-// };
-
-// int main()
-// {
-// 	string str;
-// 	char ch;
-// 	cout<<"Enter the string- ";
-// 	cin>>str;
-// 	cout<<"Enter the character to find the occurence of that character in that string- ";
-// 	cin>>ch;
-// 	Occurence obj;
-// 	obj.search(str,ch);
-//     return 0;
-// }
-
 #include <iostream>
 using namespace std;
 class University
@@ -98,39 +62,3 @@ int main()
 
 	return 0;
 }
-
-// #include <iostream>
-// using namespace std;
-
-// int maxmarks(string dept_name)
-// {
-
-// 	int marks[10], i, max = 0;
-
-// 	cout << "Enter the marks of the students of " << dept_name << ": ";
-// 	for (i = 0; i < 10; i++)
-// 	{
-// 		cin >> marks[i];
-// 		if (marks[i] > max)
-// 		{
-// 			max = marks[i];
-// 		}
-// 	}
-
-// 	cout << "Highest marks in the class: " << max << endl;
-
-// 	return max;
-// };
-
-// int main()
-// {
-
-// 	int SCA = maxmarks("SCA");
-
-// 	cout << "Topper of SCA: " << SCA << endl;
-
-// 	int SCS = maxmarks("SCS");
-
-// 	cout << "Topper of SCS: " << SCS << endl;
-// 	return 0;
-// }
